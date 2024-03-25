51e788de284c3fcc7f775709f69261a9d28dfc97
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-25 23:18:52 +0200

    Update views.py

--- sim/views.py
+++ sim/views.py
@@ -20,11 +20,27 @@ def start_quiz_view(request) -> HttpResponse:
 
 
 def get_questions(request, is_start=False) -> HttpResponse:
-  if is_start:
-    username = 'Anonymous'
-    question = _get_first_question(request)
+    if is_start:
+        username = 'Anonymous'
+        question = _get_first_question(request)
+    else:
+       question = _get_next_question(request)
+
+
+
 
 
 def _get_first_question(request) -> Question:
   quiz_id = request.POST['quiz_id']
   return Question.objects.filter(quiz_id=quiz_id, is_answered=False).first()
+
+
+def _get_next_question(request) -> Optional[Question]:
+    quiz_id = request.POST['quiz_id']
+    next_question = Question.objects.filter(quiz_id=quiz_id, is_answered=False).first()
+    try:
+        if next_question:
+            return next_question
+        return None
+    except Question.DoesNotExist:
+        return None
