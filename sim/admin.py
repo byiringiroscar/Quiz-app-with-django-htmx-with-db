f0878d508714d0455dd8969018fe6029cee066c5
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-25 23:03:16 +0200

    wip on questions

--- sim/admin.py
+++ sim/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Quiz
+from .models import Quiz, Question
 
 # Register your models here.
 
 
 admin.site.register(Quiz)
+admin.site.register(Question)
+
