dd11dc998c362bb31cda9ed518de07381b3827b4
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-25 23:34:40 +0200

    wip on answer

--- sim/urls.py
+++ sim/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.start_quiz_view, name='start'),
+  path('get-questions/start', views.get_questions, {'is_start': True}, name='get-questions'),
+  path('get-questions', views.get_questions, {'is_start': False}, name='get-questions'),
 ]
