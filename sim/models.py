d786377bae5ef3e594571544dc00beaba756a2fb
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-25 23:00:36 +0200

    Add question models

--- sim/models.py
+++ sim/models.py
@@ -10,3 +10,13 @@ class Quiz(models.Model):
 
 def __str__(self):
     return self.name
+
+
+class Question(models.Model):
+  quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+  text = models.CharField(max_length=300)
+  is_answered  = models.BooleanField(default=False)
+
+
+def __str__(self):
+    return f'{self.quiz.name} - {self.text[:20]}'
