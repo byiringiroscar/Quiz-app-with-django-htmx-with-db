76e2e4f83182f2af5121b41e0532c11aa7bf75c9
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-26 17:09:25 +0200

    wip on urls fixing

--- sim/urls.py
+++ sim/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
   path('', views.start_quiz_view, name='start'),
-  path('get-questions/start/', views.get_questions, {'is_start': True}, name='get-questions'),
-  path('get-questions/', views.get_questions, {'is_start': False}, name='get-questions'),
-  path('get-answer/', views.get_answer, name='get-answer'),
+  path('get-questions/start', views.get_questions, {'is_start': True}, name='get-questions'),
+  path('get-questions', views.get_questions, {'is_start': False}, name='get-questions'),
+  path('get-answer', views.get_answer, name='get-answer'),
   path('error/', views.handle_question_error, name='error'),
   path('view_records/', views.view_records, name='view_records'),
 
